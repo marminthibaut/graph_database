9b908b4ba855bf8f985606a14eabaeb9b3ea8188
Thibaut marmin <marminthibaut@gmail.com>
2012-03-22 23:26:04 +0100

    essaie fenetre affichage graph

---
+++ src/gd/app/gui/graph/AfficheImage.java
@@ -0,0 +1,23 @@
+package gd.app.gui.graph;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+class AfficheImage extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7819105802237756438L;
+	Image img;
+
+	AfficheImage(String s) {
+		img = getToolkit().getImage(s);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+	}
+}
---
+++ src/gd/app/gui/graph/RunExampleGraph.java
@@ -0,0 +1,17 @@
+package gd.app.gui.graph;
+
+/**
+ * 
+ * Test d'affichage d'une image
+ * 
+ * @author thibaut
+ * 
+ */
+public class RunExampleGraph {
+	@SuppressWarnings("javadoc")
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		GlobalConsoleFrame test = new GlobalConsoleFrame();
+
+	}
+}
